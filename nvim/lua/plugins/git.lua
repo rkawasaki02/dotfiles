6d99b424f8916d1d57df38fa6677c3b732dc2d32
rkawasaki02 <ryuji.cloud02@gmail.com>
2026-05-15 14:56:30 +0900

    feat: gitコマンドの新しく追加

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -16,12 +16,24 @@ return {
 		"tpope/vim-fugitive",
 		cmd = { "Git", "Gdiffsplit", "Gblame", "Gpush", "Gpull" },
 		keys = {
-			{ "<leader>gs", "<cmd>Git<cr>",        desc = "Git status" },
-			{ "<leader>gd", "<cmd>Gdiffsplit<cr>", desc = "Git diff" },
-			{ "<leader>gb", "<cmd>Git blame<cr>",  desc = "Git blame" },
-			{ "<leader>gp", "<cmd>Git push<cr>",   desc = "Git push" },
-			{ "<leader>gl", "<cmd>Git pull<cr>",   desc = "Git pull" },
-			{ "<leader>gc", "<cmd>Git commit<cr>", desc = "Git commit" },
+			{ "<leader>gs", "<cmd>Git<cr>",                desc = "Git status" },
+			{ "<leader>gd", "<cmd>Gdiffsplit<cr>",         desc = "Git diff" },
+			{ "<leader>gb", "<cmd>Git blame<cr>",          desc = "Git blame" },
+			{ "<leader>gp", "<cmd>Git push<cr>",           desc = "Git push" },
+			{ "<leader>gl", "<cmd>Git pull<cr>",           desc = "Git pull" },
+			{ "<leader>gc", "<cmd>Git commit<cr>",         desc = "Git commit" },
+			{ "<leader>gf", "<cmd>Git fetch<cr>",          desc = "Git fetch" },
+			{ "<leader>gr", "<cmd>Git rebase<cr>",         desc = "Git rebase" },
+			{ "<leader>ga", "<cmd>Git commit --amend<cr>", desc = "Git amend" },
+			-- 現在のブランチをoriginにpush（初回-u付き）
+			{
+				"<leader>gP",
+				function()
+					local branch = vim.fn.system("git branch --show-current"):gsub("\n", "")
+					vim.cmd("Git push -u origin " .. branch)
+				end,
+				desc = "Git push -u origin (current branch)",
+			},
 		},
 	},
 }
