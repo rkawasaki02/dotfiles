f32642dba9fe00bd7aaed8ee030cd08386d9d947
rkawasaki02 <ryuji.cloud02@gmail.com>
2026-06-03 04:56:29 +0900

    feat: lazygitを追加 (#4)

---
+++ nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"kdheepak/lazygit.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		cmd = "LazyGit",
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
+}
